f18948c5f7383a6351f49b03597bd64761840bb7
Tomas Utreras <tutreras100@gmail.com>
2021-03-06 20:51:55 -0500

    Updates

---
+++ precache-manifest.b58ada9e9d654873e721436a0fe331d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b112dcd46a2c205933e",
+    "url": "/ReactPortfolio/static/css/main.6a91908c.chunk.css"
+  },
+  {
+    "revision": "8b112dcd46a2c205933e",
+    "url": "/ReactPortfolio/static/js/main.8b112dcd.chunk.js"
+  },
+  {
+    "revision": "c77289d31edfc35cabf8",
+    "url": "/ReactPortfolio/static/js/1.c77289d3.chunk.js"
+  },
+  {
+    "revision": "aa22ff2d541828390b91",
+    "url": "/ReactPortfolio/static/js/runtime~main.aa22ff2d.js"
+  },
+  {
+    "revision": "e9c7eb2010db6ece846c38fb9c01141e",
+    "url": "/ReactPortfolio/index.html"
+  }
+];
